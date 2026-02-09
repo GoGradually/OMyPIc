2613c3a3031998b3d4de8ad1869415b1d7f43746
GoGradually <chkun3109@gmail.com>
2026-02-09 22:00:45 +0900

    feat: FeedbackPolicy 인터페이스 추가

---
+++ backend/application/src/main/java/me/go_gradually/omypic/application/feedback/policy/FeedbackPolicy.java
@@ -0,0 +1,11 @@
+package me.go_gradually.omypic.application.feedback.policy;
+
+public interface FeedbackPolicy {
+    int getSummaryMaxChars();
+
+    double getExampleMinRatio();
+
+    double getExampleMaxRatio();
+
+    int getWrongnoteSummaryMaxChars();
+}
