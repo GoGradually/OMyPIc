55d374269b188e3e5d60b8364d7b63d29e0aa0f3
GoGradually <chkun3109@gmail.com>
2026-02-09 22:22:01 +0900

    feat: RulebookPort 인터페이스 추가

---
+++ backend/application/src/main/java/me/go_gradually/omypic/application/rulebook/port/RulebookPort.java
@@ -0,0 +1,21 @@
+package me.go_gradually.omypic.application.rulebook.port;
+
+import me.go_gradually.omypic.domain.rulebook.Rulebook;
+import me.go_gradually.omypic.domain.rulebook.RulebookId;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 메타데이터 저장소 인터페이스
+ * 트랜잭션을 지원하는 저장소
+ */
+public interface RulebookPort {
+    List<Rulebook> findAll();
+
+    Optional<Rulebook> findById(RulebookId id);
+
+    Rulebook save(Rulebook rulebook);
+
+    void deleteById(RulebookId id);
+}
