8793117ec82d80a0139144326ddd6572a9edaebf
GoGradually <chkun3109@gmail.com>
2026-02-10 20:57:57 +0900

    feat: tts 기능 제거 - realtime 활용

--- backend/application/src/main/java/me/go_gradually/omypic/application/tts/model/AudioSink.java
+++
@@ -1,7 +0,0 @@
-package me.go_gradually.omypic.application.tts.model;
-
-import java.io.IOException;
-
-public interface AudioSink {
-    void write(byte[] bytes) throws IOException;
-}
--- backend/application/src/main/java/me/go_gradually/omypic/application/tts/port/TtsGateway.java
+++
@@ -1,7 +0,0 @@
-package me.go_gradually.omypic.application.tts.port;
-
-import me.go_gradually.omypic.application.tts.model.TtsCommand;
-
-public interface TtsGateway {
-    Iterable<byte[]> stream(String apiKey, TtsCommand command) throws Exception;
-}
