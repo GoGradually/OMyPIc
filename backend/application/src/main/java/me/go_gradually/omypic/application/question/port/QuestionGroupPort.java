837ad90947f599e194cf410d7dca7fe9b4ae4e14
GoGradually <chkun3109@gmail.com>
2026-02-13 00:34:23 +0900

    feat: QuestionGroupPort 인터페이스 추가 및 CRUD 메서드 정의

---
+++ backend/application/src/main/java/me/go_gradually/omypic/application/question/port/QuestionGroupPort.java
@@ -0,0 +1,20 @@
+package me.go_gradually.omypic.application.question.port;
+
+import me.go_gradually.omypic.domain.question.QuestionGroupAggregate;
+import me.go_gradually.omypic.domain.question.QuestionGroupId;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionGroupPort {
+    List<QuestionGroupAggregate> findAll();
+
+    List<QuestionGroupAggregate> findAllById(Collection<QuestionGroupId> ids);
+
+    Optional<QuestionGroupAggregate> findById(QuestionGroupId id);
+
+    QuestionGroupAggregate save(QuestionGroupAggregate group);
+
+    void deleteById(QuestionGroupId id);
+}
